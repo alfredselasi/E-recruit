9a15ce51bc15b4200fa81bec8ada5988bc2f85d0
Agada <agadap46@gmail.com>
2022-03-18 15:59:07 +0000

    Ready for deployment for v1.0

--- erecruit/settings.py
+++ erecruit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iz((^#j36$qz+%@_!(#ktoqb(ny003g@ibz#$ji(j(9+ovlu8q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','e-recruit-ghana.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
